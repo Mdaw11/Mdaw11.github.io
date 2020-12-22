4f063c6d4e42ce1151fdc824e9a29aff5146161d
Miko Dawili <mikhaeldaw99@gmail.com>
2020-12-22 08:37:54 -0600

    New update

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     })
 // This object logs strings onto the page automatically
     var typed = new Typed(".element", {
-        strings: ["I'm Miko", "I'm a Developer", "Need help with your Website?", "Have a look around!" ],
+        strings: ["I'm Mikhael", "I'm a Developer", "Need help with your Website?", "Have a look around!" ],
         smartBackspace: true,
         typeSpeed: 100,
         backSpeed: 100,
@@ -19,34 +19,31 @@ $(document).ready(function(){
     });
 
 // progress bars
-var waypoint = new Waypoint({
-    element: document.getElementById('exp-id'),
-    handler: function() {
-        var p = document.querySelectorAll('.progress-bar');
-        p[0].setAttribute("style", "width:100%;transition:1s all");
-        p[1].setAttribute("style", "width:95%;transition:1.5s all");
-        p[2].setAttribute("style", "width:85%;transition:1.7s all");
-        p[3].setAttribute("style", "width:99%;transition:2s all");
-        p[4].setAttribute("style", "width:85%;transition:2.3s all");
-        p[5].setAttribute("style", "width:95%;transition:2.5s all");        
-    },
-    offset:'90%'
-  });
+    var waypoint = new Waypoint({
+        element: document.getElementById('exp-id'),
+        handler: function() {
+            var p = document.querySelectorAll('.progress-bar');
+            p[0].setAttribute("style", "width:100%;transition:1s all");
+            p[1].setAttribute("style", "width:95%;transition:1.5s all");
+            p[2].setAttribute("style", "width:85%;transition:1.7s all");
+            p[3].setAttribute("style", "width:99%;transition:2s all");
+            p[4].setAttribute("style", "width:70%;transition:2.3s all");        
+        },
+        offset:'90%'
+    });
 
-// This will filter out work
-var filterizd =$('.filter-container').filterizr({
-    animationDuration: .5,
-})
+    // This will filter out work
+    var filterizd =$('.filter-container').filterizr({
+        animationDuration: .5,
+    })
 
-// Owl Carousel
-$(".owl-carousel").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 4000,
-    items: 1,
+    // Owl Carousel
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        items: 1,
 
-});
-
-
+    });
 
 });
